ad8e7044311d6023630536e31ddfebd4aa549efb
AlexXuan <xuanxiaoguang@gmail.com>
2023-05-10 08:59:41 +0800

    add some new points

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 '''
 Author: AlexanderXuan xuanxiaoguang@gmail.com
 Date: 2023-05-07 19:53:37
-LastEditors: AlexanderXuan xuanxiaoguang@gmail.com
-LastEditTime: 2023-05-07 20:03:00
-FilePath: /fastspi-exp/run.py
+LastEditors: AlexXuan xuanxiaoguang@gmail.com
+LastEditTime: 2023-05-10 08:57:25
+FilePath: /fastapi-exp/run.py
 '''
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from tutorial import app03, app04, app05
 
 app = FastAPI()
+
+# mount表示将某个目录下一个完全独立的应用挂在过来，这个不会在API交互文档中显示
+app.mount(path='/staticstatic', app=StaticFiles(directory='./coronavirus/static'), name='static') # 静态文件不要挂载到APIRouter中
 
 app.include_router(app03, prefix="/chapter03", tags=["第三章 请求参数和验证"])
 app.include_router(app04, prefix="/chapter04", tags=["第四章 响应处理和FastAPI配置"])
--- tutorial/chapter04.py
+++ tutorial/chapter04.py
@@ -1,12 +1,12 @@
 '''
 Author: AlexanderXuan xuanxiaoguang@gmail.com
 Date: 2023-05-07 19:54:06
-LastEditors: AlexanderXuan xuanxiaoguang@gmail.com
-LastEditTime: 2023-05-07 21:26:51
-FilePath: /fastspi-exp/tutorial/chapter04.py
+LastEditors: AlexXuan xuanxiaoguang@gmail.com
+LastEditTime: 2023-05-10 08:57:47
+FilePath: /fastapi-exp/tutorial/chapter04.py
 '''
 from typing import Optional, List, Union
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Form, File, UploadFile
 from pydantic import BaseModel, EmailStr
 
 app04 = APIRouter()
@@ -64,3 +64,64 @@ async def response_model_attributes(user: UserIn):
     """include/exclude: 仅返回指定的字段"""
     # del user.password
     return user # [user, user] for List[UserOut]
+
+
+"""Response Status Code 响应状态码"""
+
+@app04.post("/status_code", status_code=200)
+async def status_code():
+    return {"status_code": 200}
+
+@app04.post("/status_attribute", status_code=status.HTTP_200_OK)
+async def status_code():
+    print(type(status.HTTP_200_OK))
+    return {"status_code": status.HTTP_200_OK}
+
+
+"""Form Data 表单数据处理"""
+
+@app04.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):  # 定义表单参数
+    """用Form类需要pip install python-multipart; 校验类似前面的请求参数验证, Form表单和body的差异在于请求头中的content-type有差异"""
+    return {"username": username}
+
+"""Request Files 单文件、多文件上传及参数详解"""
+
+@app04.post("/file")
+async def file_(file: bytes = File(...)):   # 上传多个文件变为file: List[bytes] = File(...)
+    """使用File类 文件内容会以bytes形式读入内存 适合于上传小文件"""
+    return {"file_size": len(file)}
+
+
+@app04.post("/upload_files")
+async def upload_files(files: List[UploadFile] = File(...)):    # 上传单个文件变为file: UploadFile = File(...)
+    """使用UploadFile类的优势：
+    1. 文件存储在内存中，使用的内存达到阈值后，将被保存在磁盘中；
+    2. 适合于图片、视频大文件
+    3. 可以获取上传文件的元数据，如文件名，创建时间等
+    4. 有文件对象的异步接口（用异步方式读取或更改文件）
+    5. 上传的文件时Python文件对象，可以使用write(), read(), seek(), close()操作
+    """
+    for file in files:
+        contents = await file.read()
+        print(contents)
+    
+    return {"filename": files[0].filename, "content_type": files[0].content_type}
+
+"""【见run.py】FastAPI项目的静态文件配置"""
+
+"""Path Operation Configuration 路径操作配置"""
+
+@app04.post(
+    "/path_operation_configuration",
+    response_model=UserOut,
+    # tags=["Path", "Operation", "Configuration"],
+    summary="This is summary",
+    description="This is description",
+    response_description="This is response description",
+    # deprecated=True,
+    status_code=status.HTTP_200_OK
+)
+async def path_operation_configuration(user: UserIn):
+    """Path Operation Configuration 路径操作配置"""
+    return user.dict()
